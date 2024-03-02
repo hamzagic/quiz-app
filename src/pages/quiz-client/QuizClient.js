2f4966480946655fb8a6e5335386812ae8edde17
Francis Hamzagic <francishamzagic@gmail.com>
2024-03-01 23:41:12 -0300

    working on quiz client

--- src/pages/quiz-client/QuizClient.js
+++ src/pages/quiz-client/QuizClient.js
@@ -1,36 +1,39 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import API from '../../routes/api';
 
 const QuizClient = (props) => {
   const id = props.match.params.id || '';
-  API.get('client/'+id)
-  .then(res => {
-    console.log(res);
-  })
-  .catch(err => console.log(err));
+  const [quiz, setQuiz] = useState({});
+ 
   useEffect(() => {
-    
-  },[]);
+    API.get('client/'+id)
+    .then(res => {
+      console.log(res);
+      setQuiz(res.data.data);
+    })
+    .catch(err => console.log(err));
+  },[id]);
   return(
     <div>
-      <h1>Quiz Name</h1>
+      <h1>{quiz.quizName}</h1>
       <div>
-        <h3>Quiz Question</h3>
-        <div>
-          <p>Alternatives:</p>
-          <ul>
-            <li><span>Alternative 1</span> <input type="radio" name="alternative" id="" /></li>
-            <li><span>Alternative 2</span> <input type="radio" name="alternative" id="" /></li>
-            <li><span>Alternative 3</span> <input type="radio" name="alternative" id="" /></li>
-          </ul>
-          <div>
-            <button>Previous Question</button>
-            <button>Next Question</button>
+        {quiz.questions && quiz.questions.map((question, i) => 
+          <div key={i}>
+          <h3>{question.questionText}</h3>
+            <p>Alternatives:</p>
+              {question.answers.map((answer, index) => 
+              <ul key={index}>
+                <li><input type="radio" name={`alternative-${i}`} /><span>{answer}</span></li>
+              </ul>
+              )}
           </div>
+        )}
+        <div>
+          <button>Previous Question</button>
+          <button>Next Question</button>
         </div>
       </div>
     </div>
-
   );
 }
 
